b871253f059c653f3d04640af9a3b4459b52dcc4
nishant <nishantshetty92@gmail.com>
2023-07-06 00:40:08 +0530

    modified the project structure to dockerize the django application

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.8-alpine
+ENV PYTHONUNBUFFERED=1
+RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev curl-dev
+WORKDIR /app
+COPY app/requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
--- app/shop_surfer_python/settings.py
+++ app/shop_surfer_python/settings.py
@@ -20,7 +20,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, '.env'))
+env.read_env('../.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
