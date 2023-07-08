f051d8aabfcde93ba7529e3e0e9131387fae2c71
nishant <nishantshetty92@gmail.com>
2023-07-08 18:42:03 +0530

    using DEBUG env value correctly

--- app/shop_surfer_python/settings.py
+++ app/shop_surfer_python/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", default="secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = True if os.environ.get("DEBUG", default="False") == "True" else False
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", default="").split(",")
 
